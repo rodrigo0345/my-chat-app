f5935c836f1462b5c9ab7bea724073dd250897c0
Rodrigo Casanova <rodrigocasanova@pop-os.localdomain>
2022-09-09 19:02:34 +0100

    sign out button

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Card, Button, Alert } from 'react-bootstrap'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Dashboard() {
+    const { currentUser, logout } = useAuth();
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    async function handleLogout() {
+        
+        try{
+            setError('');
+            await logout();
+            navigate('/login');
+        }
+        catch(error){
+            setError("Failed to log out");
+            console.warn(error);
+        }
+    }
+
   return (
-    <div>Dashboard</div>
+    <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4">Profile</h2>
+                {error && <Alert className="alert alert-danger">{error}</Alert>}
+                <strong>Email: {currentUser.email}</strong>
+                <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+                    Update Profile
+                </Link>
+            </Card.Body>
+
+        </Card>
+        <div className="w-100 text-center mt-2">
+            <Button variant="link" onClick={handleLogout}>Log Out</Button>
+        </div>
+    </>
   )
 }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../firebase'
 
 const AuthContext = React.createContext();
@@ -20,6 +20,10 @@ export function AuthProvider({ children }) {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    function logout(){
+        return signOut(auth);
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user);
@@ -33,7 +37,8 @@ export function AuthProvider({ children }) {
     const value = {
         currentUser,
         signup,
-        login
+        login,
+        logout
     }
     return (
         <AuthContext.Provider value={value}>
