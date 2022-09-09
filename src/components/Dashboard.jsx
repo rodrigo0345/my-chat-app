60633c4b95875880138a589b72da45660685ac33
Rodrigo Casanova <rodrigocasanova@pop-os.localdomain>
2022-09-09 23:15:04 +0100

    styling the profile picture

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import { Card, Button, Alert } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
-import { PrivateRoute } from './PrivateRoute'
 
 export default function Dashboard() {
     const { currentUser, logout } = useAuth();
@@ -25,10 +24,11 @@ export default function Dashboard() {
   return (
     <>
         <Card>
-            <Card.Body>
+            <Card.Body className="d-flex flex-column">
                 <h2 className="text-center mb-4">Profile</h2>
                 {error && <Alert className="alert alert-danger">{error}</Alert>}
-                <img src={currentUser?.photoURL? currentUser.photoURL: "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"} alt="" />
+                <img src={currentUser?.photoURL? currentUser.photoURL: "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"} alt="" style={{margin: "auto", border: "1px solid black", padding: "0.4em"}} height="150"/>
+                <br />
                 <strong>Username: {currentUser && currentUser.displayName}</strong>
                 <br />
                 <strong>Email: {currentUser && currentUser.email}</strong>
--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -1,8 +1,7 @@
 import React, {useRef, useState} from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
-import { createRoutesFromChildren, Link, useNavigate } from 'react-router-dom';
-import { updateProfile } from 'firebase/auth';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function UpdateProfile() {
     const emailRef = useRef();
