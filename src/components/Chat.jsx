13bb11b351595124ab0d82478fe1ac2d27883ddc
Rodrigo Casanova <rodrigocasanova@pop-os.localdomain>
2022-09-10 22:33:23 +0100

    adding some styling

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useMsg } from '../contexts/MsgContext'
 import { Form, Button, Card, Alert, Container } from 'react-bootstrap'
+import "../styles/Chat.css"
 
 export default function Chat() {
   const getMessages = useMsg();
   const sendMessage = useMsg();
 
+  async function send(e){
+    e.preventDefault();
+  }
+
+  function get(){
+
+  }
 
   return (
     <>
@@ -15,11 +23,11 @@ export default function Chat() {
         <p className="my-msg">Adeus</p>
       </div>
       <div className="send-message">
-        <Form>
+        <Form className='d-flex align-items-center' onSubmit={send}>
           <Form.Group id="message">
-            <Form.Control className="mb-3" type="text" required />
+            <Form.Control type="text" required />
           </Form.Group>
-          <Button type="submit" className='w-100 mt-3'>
+          <Button type="submit" className='w-40'>
             Send
           </Button>
         </Form>
